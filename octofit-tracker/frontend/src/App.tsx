264ed4fb9b7fb8386252402177b8053f29415793
Acc-khasim <syed.shabeer.haseena@accenture.com>
2026-06-10 16:33:49 +0300

    Add App component

---
+++ octofit-tracker/frontend/src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className="app">
+      <h1>OctoFit Tracker</h1>
+      <p>Build your fitness journey with GitHub Copilot</p>
+    </div>
+  )
+}
+
+export default App
